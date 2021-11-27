7feb3a99df130f4e53ca40296bd312ab2a286d3c
”Dunnikov <dynnikovleha@gmail.com>
2021-11-27 11:36:45 +0300

    added hamburger

--- js/main.js
+++ js/main.js
@@ -46,9 +46,9 @@ $(function () {
 
 //gamburger
 let menuBtn = document.querySelector(".header__hamburger-btn");
-//let aside = document.querySelector(".menu");
+let aside = document.querySelector(".header__aside-menu");
 menuBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   menuBtn.classList.toggle("header__hamburger-btn--active");
-  //aside.classList.toggle("menu--active");
+  aside.classList.toggle("header__aside-menu--active");
 });
